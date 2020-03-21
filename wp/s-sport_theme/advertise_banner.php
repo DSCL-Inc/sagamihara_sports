8fcdb4a72b061a2343e05805c1ed8970af0997f0
Takashi Kobashi <koba@dscl.jp>
2020-03-21 14:35:11 +0900

    バナー表示を固定ページ(id=12)から読み込むよう修正

--- wp/s-sport_theme/advertise_banner.php
+++ wp/s-sport_theme/advertise_banner.php
@@ -3,9 +3,8 @@
             <div class="p-top-section__head">
               <h4 class="p-top-section__head__title">
                 <span
-                  class="p-top-section__head__character p-top-section__head__character--ad"
-                ></span
-                >広告バナー
+                  class="p-top-section__head__character p-top-section__head__character--ad">
+                  </span>広告バナー
               </h4>
               <a class="p-top-head__btn u-shadow" href="<?php echo esc_url( home_url( '/' ) ); ?>banner-guide"
                 >詳しく見る<span class="u-icon-right-angle"></span
@@ -13,26 +12,37 @@
             </div>
             <div class="l-content u-shadow">
               <div class="p-top-advertisement__list">
-              <?php $i=0; ?>
-        <?php if(have_rows('top_advertisement')):?>
-              <?php while(have_rows('top_advertisement')):the_row(); ?>
-              <?php $top_advertisement=get_sub_field('top_advertisement_group'); ?>
-              <?php if($top_advertisement): ?>
-                <a href="<?php echo $top_advertisement['top_advertisement_url']; ?>" class="p-top-advertisement__list__item">
-                  <img src="<?php echo $top_advertisement['top_advertisement_img'];?>" alt="<?php echo $top_advertisement['top_advertisement_name']; ?>"/>
-                </a>
-                
-              <?php endif; ?>
-              <?php $i++;?>
-              <?php endwhile; ?>
-              <?php endif; ?>
-            <?php if($i < 10): ?>
-                <?php for($i = $i; $i < 10; $i++):?>
-              <a class="p-top-advertisement__list__item">
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/image/p-top-advertisement_dummy.png" />
-                </a>
-              <?php endfor; ?>
-        <?php endif; ?>
+                <?php
+                  $i=0;
+                  $vacant_img_url = get_template_directory_uri()."/assets/image/p-top-advertisement_dummy.png"; //空き枠画像URL
+                  if(have_rows('top_advertisement',12)):
+                    while(have_rows('top_advertisement',12)):the_row();
+                      $top_advertisement=get_sub_field('top_advertisement_group'); 
+                      if($top_advertisement):
+//ヒアドキュメント開始
+echo <<<EOT
+<a href="{$top_advertisement['top_advertisement_url']}" target="_blank" rel="noopener" class="p-top-advertisement__list__item">
+<img src="{$top_advertisement['top_advertisement_img']}" alt="{$top_advertisement['top_advertisement_name']} ?>"/>
+</a>
+EOT;
+//ヒアドキュメント終了
+
+                      endif;
+                      $i++;
+                    endwhile;
+                  endif;
+                  if($i < 10):
+                    for($i = $i; $i < 10; $i++):
+//ヒアドキュメント開始
+echo <<<EOT
+<a class="p-top-advertisement__list__item">
+  <img src="{$vacant_img_url}" />
+</a>
+EOT;
+//ヒアドキュメント終了
+                  endfor; 
+                endif;
+                ?>
               </div>
             </div>
           </section>
