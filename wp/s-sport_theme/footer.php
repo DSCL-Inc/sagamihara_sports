5b25d745cbc4528e3cf52710d43b427cba13806d
Takashi Kobashi <koba@dscl.jp>
2020-03-21 14:48:06 +0900

    #79 フッターの表記をヘッダーに合わせる修正＋コード整形

--- wp/s-sport_theme/footer.php
+++ wp/s-sport_theme/footer.php
@@ -3,59 +3,55 @@
     <div class="l-footer-top__column l-wrap">
       <div class="l-footer-top__column__item">
         <ul class="l-footer-top__list">
-          <li class="l-footer-top__list__item"><span class="u-icon-header-menu--about"></span>スポーツ協会について</li>
+          <li class="l-footer-top__list__item"><span class="u-icon-header-menu--about"></span>スポーツ協会</li>
           <li class="l-footer-top__list__item">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>about" class="l-footer-top__list__link">スポーツ協会とは</a></li>
-          <li class="l-footer-top__list__item">            <a href="<?php echo esc_url( home_url( '/' ) ); ?>facility" class="l-footer-top__list__link">
-            指定管理者施設</a></li>
-			<li class="l-footer-top__list__item"><a href="<?php echo esc_url( home_url( '/' ) ); ?>access" class="l-footer-top__list__link">
-           営業・アクセス</a></li>
+            <a href="<?php echo esc_url(home_url('/')); ?>about" class="l-footer-top__list__link">スポーツ協会について</a></li>
+          <li class="l-footer-top__list__item"> <a href="<?php echo esc_url(home_url('/')); ?>facility" class="l-footer-top__list__link">
+              指定管理者施設</a></li>
+          <li class="l-footer-top__list__item"><a href="<?php echo esc_url(home_url('/')); ?>access" class="l-footer-top__list__link">
+              営業・アクセス</a></li>
         </ul>
       </div>
       <div class="l-footer-top__column__item">
         <ul class="l-footer-top__list">
           <li class="l-footer-top__list__item">
-            <span class="u-icon-header-menu--event"></span>イベント情報
-        </li>
+            <span class="u-icon-header-menu--event"></span>イベント情報</li>
           <li class="l-footer-top__list__item">
             <a href="" class="l-footer-top__list__link">協会事業</a></li>
           <li class="l-footer-top__list__item">
-              <a href="<?php echo esc_url( home_url( '/' ) ); ?>tournament" class="l-footer-top__list__link">
-            市民選手権大会情報</a></li>
+            <a href="<?php echo esc_url(home_url('/')); ?>tournament" class="l-footer-top__list__link">
+              市民選手権大会</a></li>
         </ul>
       </div>
       <div class="l-footer-top__column__item">
         <ul class="l-footer-top__list">
           <li class="l-footer-top__list__item">
-            <span class="u-icon-header-menu--junior"></span>スポーツ少年団
+            <span class="u-icon-header-menu--junior"></span>スポーツ少年団</li>
+          <li class="l-footer-top__list__item">
+            <a href="<?php echo esc_url(home_url('/')); ?>junior" class="l-footer-top__list__link">
+              スポーツ少年団とは</a></li>
+          <li class="l-footer-top__list__item">
+            <a href="<?php echo esc_url(home_url('/')); ?>junior/register" class="l-footer-top__list__link">
+              登録・更新方法
+            </a></li>
+          <li class="l-footer-top__list__item">
+            <a href="<?php echo esc_url(home_url('/')); ?>junior/member" class="l-footer-top__list__link">
+              登録団
+            </a>
           </li>
           <li class="l-footer-top__list__item">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>junior" class="l-footer-top__list__link">
-                スポーツ少年団とは</a></li>
-          <li class="l-footer-top__list__item">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>junior/register" class="l-footer-top__list__link">
-  
-            新規登録入団方法
-        </a></li>
-          <li class="l-footer-top__list__item">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>junior/member" class="l-footer-top__list__link">
-  
-            登録団
-        </a>
-        </li>
-          <li class="l-footer-top__list__item">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>junior/schejule" class="l-footer-top__list__link">
-            年間行事
+            <a href="<?php echo esc_url(home_url('/')); ?>junior/schejule" class="l-footer-top__list__link">
+              年間行事予定
             </a>
-        </li>
+          </li>
         </ul>
       </div>
       <div class="l-footer-top__column__item">
         <ul class="l-footer-top__list">
           <li class="l-footer-top__list__item">
-			   <a href="<?php echo esc_url( home_url( '/' ) ); ?>member" class="l-footer-top__list__link">
-            <span class="u-icon-header-menu--member"></span>加盟団体
-			  </a>
+            <a href="<?php echo esc_url(home_url('/')); ?>member" class="l-footer-top__list__link">
+              <span class="u-icon-header-menu--member"></span>加盟団体
+            </a>
           </li>
         </ul>
         <ul class="l-footer-top__list">
@@ -92,43 +88,42 @@
   <div class="l-footer-bottom">
     <div class="l-wrap u-flex">
       <a href="#backtotop" class="c-backtotop"><span class="u-icon-top-angle"></span></a>
-    <div class="l-footer-bottom__left">
-      <a href="/" class="l-footer-bottom__logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/image/l-footer-logo.png" />
-        
-      </a>
-      <p class="l-footer-bottom__address">
-        〒252-0236　相模原市中央区富士見6-6-23　けやき会館4階
-        <br />TEL:042-751-5552 FAX:042-751-5545
-      </p>
-    </div>
-    <div class="l-footer-bottom__right">
-      <ul class="l-footer-bottom__nav">
-        <li class="l-footer-bottom__nav__item">
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>/link" class="l-footer-bottom__nav__item__link">リンク</a>
-        </li>
-        <li class="l-footer-bottom__nav__item">
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>policy" class="l-footer-bottom__nav__item__link"
-            >個人情報保護方針</a
-          >
-        </li>
-        <li class="l-footer-bottom__nav__item">
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>/access" class="l-footer-bottom__nav__item__link">営業・アクセス</a>
-        </li>
-        <li class="l-footer-bottom__nav__item">
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>/contact" class="l-footer-bottom__nav__item__link">お問い合わせ</a>
-        </li>
-      </ul>
-      <p class="l-footer-bottom__copyright">
-        ©︎Sagamihara Sport Association All Right Reserved
-      </p>
+      <div class="l-footer-bottom__left">
+        <a href="/" class="l-footer-bottom__logo">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/image/l-footer-logo.png" />
+
+        </a>
+        <p class="l-footer-bottom__address">
+          〒252-0236　相模原市中央区富士見6-6-23　けやき会館4階
+          <br />TEL:042-751-5552 FAX:042-751-5545
+        </p>
+      </div>
+      <div class="l-footer-bottom__right">
+        <ul class="l-footer-bottom__nav">
+          <li class="l-footer-bottom__nav__item">
+            <a href="<?php echo esc_url(home_url('/')); ?>/link" class="l-footer-bottom__nav__item__link">リンク</a>
+          </li>
+          <li class="l-footer-bottom__nav__item">
+            <a href="<?php echo esc_url(home_url('/')); ?>policy" class="l-footer-bottom__nav__item__link"            >個人情報保護方針</a>
+          </li>
+          <li class="l-footer-bottom__nav__item">
+            <a href="<?php echo esc_url(home_url('/')); ?>/access" class="l-footer-bottom__nav__item__link">営業・アクセス</a>
+          </li>
+          <li class="l-footer-bottom__nav__item">
+            <a href="<?php echo esc_url(home_url('/')); ?>/contact" class="l-footer-bottom__nav__item__link">お問い合わせ</a>
+          </li>
+        </ul>
+        <p class="l-footer-bottom__copyright">
+          ©︎Sagamihara Sport Association All Right Reserved
+        </p>
+      </div>
     </div>
   </div>
-</div>
 
-			</footer><!-- #site-footer -->
+</footer><!-- #site-footer -->
 </div><!-- l-container -->
-		
+
 <?php wp_footer(); ?>
-	</body>
+</body>
+
 </html>
