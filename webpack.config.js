4fb3e8738c92171fdd7b4aa7890f9fcec2c3bd11
浅見　要介 <asami@dscl.jp>
2021-06-07 12:26:27 +0900

    no message

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: path.resolve(__dirname, "./src/js/index.js"),
-  output: {
-    path: path.resolve(__dirname, "./site/common/js"),
-    filename: "bundle.js"
-  }
-};
